b5c3ba481fce18d5cd2c43f1262642a59022fd2c
Dauryen-07 <dauryenbauyrjan@gmail.com>
2025-06-08 20:37:30 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const noBtn = document.getElementById('noBtn');
+
+noBtn.addEventListener('mouseover', () => {
+    const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
+    const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
+    noBtn.style.position = 'absolute';
+    noBtn.style.left = `${x}px`;
+    noBtn.style.top = `${y}px`;
+});
